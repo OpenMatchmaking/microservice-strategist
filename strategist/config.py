a2ec90f7009342c637715399b12bd95104092e09
Valeryi Savich <relrin78@gmail.com>
2018-04-01 01:05:41 +0300

    Added default config for application

---
+++ strategist/config.py
@@ -0,0 +1,23 @@
+import os
+
+
+def to_int(value):
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        return None
+
+
+APP_WORKERS = int((os.environ.get('APP_WORKERS', 1)))
+APP_GAME_MODES_CONFIGURATION = {
+    'duel': {
+        'teams': 2,
+        'team_size': 1,
+        'algorithm': 'SimpleAverage'
+    },
+    'team-deathmatch': {
+        'teams': 2,
+        'team_size': 1,
+        'algorithm': 'SimpleAverage'
+    }
+}
