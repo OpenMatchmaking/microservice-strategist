0338aefe70ae29a65d9adc8fb8853961a680f9c6
Valeryi Savich <relrin78@gmail.com>
2018-04-02 15:40:48 +0300

    Added AMQP worker to the main application

--- strategist/run.py
+++ strategist/run.py
@@ -1,5 +1,10 @@
 from app.application import App
+from app.amqp.extension import AmqpExtension
+from app.amqp.worker import MatchCheckWorker
 
 
 app = App()
+app.amqp = AmqpExtension(app)
+app.amqp.register_worker(MatchCheckWorker(app))
+
 app.run()
