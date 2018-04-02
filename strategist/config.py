156929f0d8f185d840f24ddda1103b3d7cf61543
Valeryi Savich <relrin78@gmail.com>
2018-04-02 15:38:58 +0300

    Added configuration for AMQP

--- strategist/config.py
+++ strategist/config.py
@@ -1,6 +1,10 @@
 import os
 
 
+def to_bool(value):
+    return str(value).strip().lower() in ['1', 'true', 'yes']
+
+
 def to_int(value):
     try:
         return int(value)
@@ -21,3 +25,11 @@ APP_GAME_MODES_CONFIGURATION = {
         'algorithm': 'SimpleAverage'
     }
 }
+
+# AMQP settings
+AMQP_USERNAME = os.environ.get("AMQP_USERNAME", "user")
+AMQP_PASSWORD = os.environ.get("AMQP_PASSWORD", "password")
+AMQP_HOST = os.environ.get("AMQP_HOST", "rabbitmq")
+AMQP_PORT = to_int(os.environ.get("AMQP_PORT", 5672))
+AMQP_VIRTUAL_HOST = os.environ.get("AMQP_VIRTUAL_HOST", "vhost")
+AMQP_USING_SSL = to_bool(os.environ.get("AMQP_USING_SSL", False))
