e536a81e846c38d1ad36ee78f11c2d72cac0012b
Valeryi Savich <relrin78@gmail.com>
2018-04-07 18:09:21 +0300

    Updated code after migrating to the latest release of sage-utils

--- strategist/app/workers/microservice_register.py
+++ strategist/app/workers/microservice_register.py
@@ -1,12 +1,7 @@
-from sage_utils.amqp.base import AmqpWorker
-from sage_utils.amqp.clients import RpcAmqpClient
+from sage_utils.amqp.workers import BaseRegisterWorker
 
 
-class MicroserviceRegisterWorker(AmqpWorker):
-    REQUEST_QUEUE_NAME = "auth.microservices.register"
-    REQUEST_EXCHANGE_NAME = "open-matchmaking.direct"
-    RESPONSE_EXCHANGE_NAME = "open-matchmaking.responses.direct"
-    CONTENT_TYPE = 'application/json'
+class MicroserviceRegisterWorker(BaseRegisterWorker):
 
     def get_microservice_data(self, app):
         return {
@@ -14,17 +9,3 @@ class MicroserviceRegisterWorker(AmqpWorker):
             'version': self.app.config['SERVICE_VERSION'],
             'permissions': []
         }
-
-    async def run(self, *args, **kwargs):
-        client = RpcAmqpClient(
-            self.app,
-            routing_key=self.REQUEST_QUEUE_NAME,
-            request_exchange=self.REQUEST_EXCHANGE_NAME,
-            response_queue='',
-            response_exchange=self.RESPONSE_EXCHANGE_NAME
-        )
-        response = await client.send(self.get_microservice_data(self.app))
-
-        assert 'error' not in response.keys(), response['error']
-        assert 'content' in response.keys()
-        assert response['content'] == 'OK'
